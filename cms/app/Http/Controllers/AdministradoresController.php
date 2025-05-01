5e13aca9d536b4d9bf8bad4384bb37e5f06f35a3
Octavio <octaviohaurigot@gmail.com>
2025-05-01 19:59:49 -0300

    Editar administradores creaedo y configurado

--- cms/app/Http/Controllers/AdministradoresController.php
+++ cms/app/Http/Controllers/AdministradoresController.php
@@ -8,6 +8,9 @@ use App\Models\Blog;
 
 class AdministradoresController extends Controller
 {
+    /*======================================
+            MOSTRAR TODOS LOS REGISTROS
+    ========================================*/
     public function index()
     {
         $administradores = Administradores::all();
@@ -15,4 +18,21 @@ class AdministradoresController extends Controller
 
         return view('paginas.administradores', array("administradores" => $administradores, 'blog' => $blog));
     }
+    /*======================================
+            MOSTRAR UN REGISTRO
+    ========================================*/
+    public function show($id)
+    {
+        $administrador = Administradores::where("id", $id) ->get();
+        $blog = Blog::all();
+
+        if(count($administrador) != 0){
+            return view(    'paginas.administradores', array("status" => 200, "administradores" => $administrador, 'blog' => $blog));
+        }
+        else
+        {
+            return view(    'paginas.administradores', array("status" => 404));
+        }
+    }
+
 }
